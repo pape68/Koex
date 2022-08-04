b220e9941f4fda113cf7759f92cf6b801a3fedd8
Koji Odyssey <cldoc507@gmail.com>
2022-08-04 10:23:33 -0400

    duct tape

--- src/commands/fortnite/modals/auth.ts
+++ src/commands/fortnite/modals/auth.ts
@@ -45,74 +45,75 @@ const execute: Modal = async (client: Bot, interaction: ModalSubmitInteraction) 
                 }
             });
 
-            const deviceAuthRes = await bearerInstance.post(
-                `/account/api/public/account/${account_id}/deviceAuth`,
-                {}
-            );
+            await bearerInstance
+                .post(`/account/api/public/account/${account_id}/deviceAuth`, {})
+                .then(async (res) => {
+                    const device_id = res.data.deviceId;
+                    const secret = res.data.secret;
 
-            const device_id = deviceAuthRes.data.deviceId;
-            const secret = deviceAuthRes.data.secret;
+                    const deviceAuthParams = new URLSearchParams({
+                        grant_type: 'device_auth',
+                        account_id,
+                        device_id,
+                        secret
+                    });
 
-            const deviceAuthParams = new URLSearchParams({
-                grant_type: 'device_auth',
-                account_id,
-                device_id,
-                secret
-            });
+                    const tokenRes = await basicInstance.post(
+                        `/account/api/oauth/token`,
+                        deviceAuthParams.toString()
+                    );
 
-            const tokenRes = await basicInstance.post(
-                `/account/api/oauth/token`,
-                deviceAuthParams.toString()
-            );
+                    const { error } = await client.supabase
+                        .from('fortnite')
+                        .upsert({
+                            user_id: interaction.user.id,
+                            access_token: tokenRes.data.access_token,
+                            account_id,
+                            device_id,
+                            secret
+                        })
+                        .single();
 
-            const { error } = await client.supabase
-                .from('fortnite')
-                .upsert({
-                    user_id: interaction.user.id,
-                    access_token: tokenRes.data.access_token,
-                    account_id,
-                    device_id,
-                    secret
-                })
-                .single();
-
-            if (error) {
-                console.error(error);
-                return interaction.editReply('An interal occurred. Please try again.');
-            }
-
-            const newLoginUrl = `https://www.epicgames.com/id/login?redirectUrl=https%3A%2F%2Fwww.epicgames.com%2Fid%2Fapi%2Fredirect%3FclientId%${fortniteIOSGameClient.id}%26responseType%3Dcode%0A&prompt=login`;
-            interaction.editReply({
-                content: `You have been logged in as **${displayName}**. [Click here to switch accounts](${newLoginUrl}).`,
-                components: [
-                    {
-                        type: ComponentType.ActionRow,
-                        components: [client.interactions.get('login')!.options]
+                    if (error) {
+                        console.error(error);
+                        return interaction.editReply('An interal occurred. Please try again.');
                     }
-                ]
-            });
 
-            const embed = menuEmbed.setAuthor({
-                name: displayName,
-                iconURL: interaction.user.avatarURL() ?? undefined
-            });
+                    const newLoginUrl = `https://www.epicgames.com/id/login?redirectUrl=https%3A%2F%2Fwww.epicgames.com%2Fid%2Fapi%2Fredirect%3FclientId%${fortniteIOSGameClient.id}%26responseType%3Dcode%0A&prompt=login`;
+                    interaction.editReply({
+                        content: `You have been logged in as **${displayName}**. [Click here to switch accounts](${newLoginUrl}).`,
+                        components: [
+                            {
+                                type: ComponentType.ActionRow,
+                                components: [client.interactions.get('login')!.options]
+                            }
+                        ]
+                    });
 
-            interaction.user.createDM().then(() => {
-                interaction.message?.edit({
-                    embeds: [embed],
-                    components: menuComponents.loggedIn
+                    const embed = menuEmbed.setAuthor({
+                        name: displayName,
+                        iconURL: interaction.user.avatarURL() ?? undefined
+                    });
+
+                    interaction.user.createDM().then(() => {
+                        interaction.message?.edit({
+                            embeds: [embed],
+                            components: menuComponents.loggedIn
+                        });
+                    });
+                })
+                .catch((err) => {
+                    console.error(err);
+                    return interaction.editReply('An error occurred. Please try again.');
                 });
-            });
         })
         .catch((err) => {
+            client.logger.error(err);
             if (err.response && err.response.data.numericErrorCode === 18059) {
                 return interaction.editReply(
                     'You have entered an invalid code. Please try again as it may have expired.'
                 );
             }
-        })
-        .catch((err) => {
-            client.logger.error(err);
             return interaction.editReply(`Authentication failed.`);
         });
 };
