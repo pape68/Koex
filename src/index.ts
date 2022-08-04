e2413be07ed8f6075166c5acc590e83dab8aba81
Koji Odyssey <cldoc507@gmail.com>
2022-08-04 15:44:25 -0400

    exceptions, yum

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ import Bot from './structures/Bot';
 export const bot = new Bot(CLIENT_OPTIONS);
 
 bot.start();
+
+process.on('uncaughtException', (err) => {
+    bot.logger.error(`Caught exception: ${err}`);
+});
