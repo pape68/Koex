4ce230889f62ed03175fc1322d460b87bc629b3f
Koji Odyssey <cldoc507@gmail.com>
2022-08-07 15:48:30 -0700

    fixBuilds

--- src/commands/fortnite/fixBuilds.ts
+++ src/commands/fortnite/fixBuilds.ts
@@ -31,9 +31,82 @@ const command: Command = {
         let data: any;
 
         if (queryDatabase) {
-            data = (
-                await client.supabase.from('fortnite').select('*').eq('user_id', userId).single()
-            ).data;
+            const { data: dat, error: eqError } = await client.supabase
+                .from('fortnite')
+                .select('*')
+                .eq('user_id', userId)
+                .single();
+
+            if (dat && dat.account_id) {
+                if (eqError) {
+                    return client.logger.error(eqError);
+                }
+
+                const { account_id, device_id, secret } = data;
+
+                const baseInstance = {
+                    baseURL: 'https://account-public-service-prod.ol.epicgames.com',
+                    method: 'POST'
+                };
+
+                const basicInstance = axios.create({
+                    ...baseInstance,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        Authorization: `Basic ${Buffer.from(
+                            fortniteIOSGameClient.id + ':' + fortniteIOSGameClient.secret
+                        ).toString('base64')}`
+                    }
+                });
+
+                const deviceAuthParams = new URLSearchParams({
+                    grant_type: 'device_auth',
+                    account_id,
+                    device_id,
+                    secret
+                });
+
+                await basicInstance
+                    .post(`/account/api/oauth/token`, deviceAuthParams.toString())
+                    .then(async (res) => {
+                        const { data: datt, error: upsertError } = await client.supabase
+                            .from('fortnite')
+                            .upsert({
+                                user_id: interaction.user.id,
+                                access_token: res.data.access_token,
+                                account_id,
+                                device_id,
+                                secret
+                            })
+                            .single();
+
+                        data = datt;
+
+                        if (upsertError) {
+                            return client.logger.error(upsertError);
+                        }
+                    })
+                    .catch(async (err) => {
+                        client.logger.error(err);
+
+                        if (!interaction.deferred || !interaction.replied)
+                            await interaction.deferReply({ ephemeral: true });
+
+                        const { error: deleteError } = await client.supabase
+                            .from('fortnite')
+                            .delete()
+                            .match({
+                                user_id: interaction.user.id
+                            });
+
+                        if (deleteError) {
+                            client.logger.error(deleteError);
+                            return interaction.editReply('Failed to log you out.');
+                        }
+
+                        interaction.editReply('Failed to refresh credentials, please log back in.');
+                    });
+            }
 
             if (!data) {
                 return interaction.editReply('No account data found for that user.');
