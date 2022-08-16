44035c9f5a00112f9b279623a12a422464107368
Koji Odyssey <cldoc507@gmail.com>
2022-08-16 02:26:31 -0400

    hotfix

--- src/utils/functions/getUserData.ts
+++ src/utils/functions/getUserData.ts
@@ -5,15 +5,12 @@ import { ChatInputCommandInteraction } from 'discord.js';
 import { ComponentInteraction } from '../../interfaces/Component';
 import { Accounts } from '../../typings/supabase';
 import createEmbed from './createEmbed';
-import refreshUserData from './refreshUserData';
 
 const getUserData = async(
     userId: string,
     interaction?: ChatInputCommandInteraction | ComponentInteraction,
     slotData = true
 ) => {
-    await refreshUserData(userId, interaction);
-    
     const { data, error } = await supabase
         .from<Accounts>('accounts')
         .upsert({ user_id: userId })
