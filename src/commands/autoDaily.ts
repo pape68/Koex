35075fe99744822c65c9e2358d3e6c4c3cda3611
KojiOdyssey <cldoc507@gmail.com>
2022-09-27 02:28:32 +0000

    weird autodaily mistake

--- src/commands/autoDaily.ts
+++ src/commands/autoDaily.ts
@@ -2,7 +2,7 @@ import { ApplicationCommandType } from 'discord.js';
 
 import { Command } from '../interfaces/Command';
 import createEmbed from '../utils/commands/createEmbed';
-import { getWhitelistedUser, removeAutoDailyUser, saveAutoDailyUser } from '../utils/functions/database';
+import { getAutoDailyUser, removeAutoDailyUser, saveAutoDailyUser } from '../utils/functions/database';
 
 const command: Command = {
     name: 'autodaily',
@@ -11,7 +11,7 @@ const command: Command = {
     execute: async (interaction) => {
         await interaction.deferReply({ ephemeral: true });
 
-        const isEnabled = await getWhitelistedUser(interaction.user.id);
+        const isEnabled = await getAutoDailyUser(interaction.user.id);
 
         switch (isEnabled) {
             case true:
